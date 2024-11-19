2f7983e80eaa5f3e665837f8a8ef53ecef0b2f6f
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-19 07:14:15 +0700

    PUSH HARIINI 2

--- GameProject/GameDemo.java
+++ GameProject/GameDemo.java
@@ -1,3 +1,13 @@
+package GameProject;
+
 public class GameDemo {
-    
+    public static void main(String[] args) {
+        Player player = new Player("Hero", 10, 10, 100, 3);
+        Monster monster = new Monster("Dragon", 50, 50, 200, 5, "Red");
+
+        player.takeDamage(20);
+        monster.takeDamage(50);
+        System.out.println(monster.makeNoise());
+        monster.takeDamage(150);
+    }
 }
--- GameProject/Monster.java
+++ GameProject/Monster.java
@@ -1,3 +1,21 @@
-public class Monster {
-    
+package GameProject;
+
+public class Monster extends DamageableObject {
+    int threatLevel;
+    String color;
+
+    public Monster(String name, int posX, int posY, int maxHealth, int threatLevel, String color) {
+        super(name, posX, posY, maxHealth);
+        this.threatLevel = threatLevel;
+        this.color = color;
+    }
+
+    public String makeNoise() {
+        return name + " roars threateningly!";
+    }
+
+    @Override
+    public void onKilled() {
+        System.out.println(name + " has been defeated!");
+    }
 }
